53837969b0fe45dfb28e3e8423e1520acff2facf
codeworkx <codeworkx@cyanogenmod.org>
2013-01-07 17:48:09 +0100

    update for ubuntu 12.10, drop 32 bit support

--- build.sh
+++ build.sh
@@ -5,31 +5,46 @@ EXTRACMD="$2"
 A_TOP=${PWD}
 CUR_DIR=`dirname $0`
 DATE=$(date +%D)
+MACHINE_TYPE=`uname -m`
 CM_VERSION=10.1
 
 # Common defines (Arch-dependent)
 case `uname -s` in
-	Darwin)
-		txtrst='\033[0m'  # Color off
-		txtred='\033[0;31m' # Red
-		txtgrn='\033[0;32m' # Green
-		txtylw='\033[0;33m' # Yellow
-		txtblu='\033[0;34m' # Blue
-		THREADS=`sysctl -an hw.logicalcpu`
-		;;
-	*)
-		txtrst='\e[0m'  # Color off
-		txtred='\e[0;31m' # Red
-		txtgrn='\e[0;32m' # Green
-		txtylw='\e[0;33m' # Yellow
-		txtblu='\e[0;34m' # Blue
-		THREADS=`cat /proc/cpuinfo | grep processor | wc -l`
-		;;
+    Darwin)
+        txtrst='\033[0m'  # Color off
+        txtred='\033[0;31m' # Red
+        txtgrn='\033[0;32m' # Green
+        txtylw='\033[0;33m' # Yellow
+        txtblu='\033[0;34m' # Blue
+        THREADS=`sysctl -an hw.logicalcpu`
+        ;;
+    *)
+        txtrst='\e[0m'  # Color off
+        txtred='\e[0;31m' # Red
+        txtgrn='\e[0;32m' # Green
+        txtylw='\e[0;33m' # Yellow
+        txtblu='\e[0;34m' # Blue
+        THREADS=`cat /proc/cpuinfo | grep processor | wc -l`
+        ;;
 esac
 
 check_root() {
     if [ ! $( id -u ) -eq 0 ]; then
         echo -e "${txtred}Please run this script as root."
+        echo -e "\r\n ${txtrst}"
+        exit
+    fi
+}
+
+check_machine_type() {
+    echo "Checking machine architecture..."
+    if [ ${MACHINE_TYPE} == 'x86_64' ]; then
+        echo -e "${txtgrn}Detected: ${MACHINE_TYPE}. Good!"
+        echo -e "\r\n ${txtrst}"
+    else
+        echo -e "${txtred}Detected: ${MACHINE_TYPE}. Bad!"
+        echo -e "${txtred}Sorry, we do only support building on 64-bit machines."
+        echo -e "${txtred}32-bit is soooo 1970, consider a upgrade. ;-)"
         echo -e "\r\n ${txtrst}"
         exit
     fi
@@ -44,137 +59,92 @@ install_sun_jdk()
 
 install_arch_packages()
 {
-    case $arch in
-    "1")
-        # i686
-        pacman -S openjdk6 perl git gnupg flex bison gperf zip unzip sdl wxgtk \
-        squashfs-tools ncurses libpng zlib libusb libusb-compat readline schedtool \
-        optipng python2 perl-switch
-        ;;
-    "2")
-        # x86_64
-        pacman -S openjdk6 perl git gnupg flex bison gperf zip unzip sdl wxgtk \
-        squashfs-tools ncurses libpng zlib libusb libusb-compat readline schedtool \
-        optipng python2 perl-switch lib32-zlib lib32-ncurses lib32-readline \
-        gcc-libs-multilib gcc-multilib lib32-gcc-libs binutils-multilib libtool-multilib
-        ;;
-    *)
-        # no arch
-        echo -e "${txtred}No arch defined, aborting."
-        echo -e "\r\n ${txtrst}"
-        exit
-        ;;
-    esac
+    # x86_64
+    pacman -S openjdk6 perl git gnupg flex bison gperf zip unzip sdl wxgtk \
+    squashfs-tools ncurses libpng zlib libusb libusb-compat readline schedtool \
+    optipng python2 perl-switch lib32-zlib lib32-ncurses lib32-readline \
+    gcc-libs-multilib gcc-multilib lib32-gcc-libs binutils-multilib libtool-multilib
 }
 
 install_ubuntu_packages()
 {
-    case $arch in
-    "1")
-        # i686
-        apt-get install git-core gnupg flex bison gperf build-essential \
-        zip curl zlib1g-dev libc6-dev libncurses5-dev x11proto-core-dev \
-        libx11-dev libreadline6-dev libgl1-mesa-dev tofrodos python-markdown \
-        libxml2-utils xsltproc pngcrush
-        ;;
-    "2")
-        # x86_64
-        apt-get install git-core gnupg flex bison gperf build-essential \
-        zip curl zlib1g-dev libc6-dev lib32ncurses5-dev ia32-libs \
-        x11proto-core-dev libx11-dev lib32z-dev \
-        libgl1-mesa-dev g++-multilib mingw32 tofrodos python-markdown \
-        libxml2-utils xsltproc pngcrush
-        ;;
-    *)
-        # no arch
-        echo -e "${txtred}No arch defined, aborting."
-        echo -e "\r\n ${txtrst}"
-        exit
-        ;;
-    esac
+    # x86_64       
+    apt-get install git-core gnupg flex bison gperf build-essential \
+    zip curl libc6-dev libncurses5-dev:i386 x11proto-core-dev \
+    libx11-dev:i386 libreadline6-dev:i386 libgl1-mesa-glx:i386 \
+    libgl1-mesa-dev g++-multilib mingw32 openjdk-6-jdk tofrodos \
+    python-markdown libxml2-utils xsltproc zlib1g-dev:i386 pngcrush
 }
 
 prepare_environment()
 {
-    echo "Which distribution are you running?"
-    echo "1) Ubuntu 10.04"
-    echo "2) Ubuntu 10.10"
-    echo "3) Ubuntu 11.04"
-    echo "4) Ubuntu 11.10"
-    echo "5) Ubuntu 12.04"
-    echo "6) Arch Linux"
-    echo "7) Debian"
+    echo "Which 64-bit distribution are you running?"
+    echo "1) Ubuntu 11.04"
+    echo "2) Ubuntu 11.10"
+    echo "3) Ubuntu 12.04"
+    echo "4) Ubuntu 12.10"
+    echo "5) Arch Linux"
+    echo "6) Debian"
     read -n1 distribution
-    echo -e "\r\n"
-    
-    echo "Arch?"
-    echo "1) i686"
-    echo "2) x86_64"
-    read -n1 arch
     echo -e "\r\n"
 
     case $distribution in
     "1")
-        # Ubuntu 10.04
-        echo "Installing packages for Ubuntu 10.04"
-        install_sun_jdk
-        install_ubuntu_packages
-        ;;
-    "2")
-        # Ubuntu 10.10
-        echo "Installing packages for Ubuntu 10.10"
-        install_sun_jdk
-        install_ubuntu_packages
-        ln -s /usr/lib32/mesa/libGL.so.1 /usr/lib32/mesa/libGL.so
-        ;;
-    "3")
         # Ubuntu 11.04
         echo "Installing packages for Ubuntu 11.04"
         install_sun_jdk
-        install_ubuntu_packages
+        apt-get install git-core gnupg flex bison gperf build-essential \
+        zip curl zlib1g-dev libc6-dev lib32ncurses5-dev ia32-libs \
+        x11proto-core-dev libx11-dev lib32readline5-dev lib32z-dev \
+        libgl1-mesa-dev g++-multilib mingw32 tofrodos python-markdown \
+        libxml2-utils xsltproc libx11-dev:i386
         ;;
-    "4")
+    "2")
         # Ubuntu 11.10
         echo "Installing packages for Ubuntu 11.10"
         install_sun_jdk
-        install_ubuntu_packages
-        apt-get install libx11-dev:i386
+        apt-get install git-core gnupg flex bison gperf build-essential \
+        zip curl zlib1g-dev libc6-dev lib32ncurses5-dev ia32-libs \
+        x11proto-core-dev libx11-dev lib32readline5-dev lib32z-dev \
+        libgl1-mesa-dev g++-multilib mingw32 tofrodos python-markdown \
+        libxml2-utils xsltproc libx11-dev:i386
         ;;
-    "5")
+    "3")
         # Ubuntu 12.04
         echo "Installing packages for Ubuntu 12.04"
-        apt-get update
-        apt-get install git-core gnupg flex bison gperf build-essential \
-        zip curl libc6-dev libncurses5-dev:i386 x11proto-core-dev \
-        libx11-dev:i386 libreadline6-dev:i386 libgl1-mesa-dev:i386 \
-        g++-multilib mingw32 openjdk-6-jdk tofrodos python-markdown \
-        libxml2-utils xsltproc zlib1g-dev:i386 pngcrush
+        install_ubuntu_packages
+        ln -s /usr/lib/i386-linux-gnu/mesa/libGL.so.1 /usr/lib/i386-linux-gnu/libGL.so
         ;;
-    
-    "6")
+    "4")
+        # Ubuntu 12.10
+        echo "Installing packages for Ubuntu 12.10"
+        install_ubuntu_packages
+        ln -s /usr/lib/i386-linux-gnu/mesa/libGL.so.1 /usr/lib/i386-linux-gnu/libGL.so
+        ;;
+    "5")
         # Arch Linux
         echo "Installing packages for Arch Linux"
         install_arch_packages
         mv /usr/bin/python /usr/bin/python.bak
         ln -s /usr/bin/python2 /usr/bin/python
         ;;
-    "7")
+    "6")
         # Debian
         echo "Installing packages for Debian"
         apt-get update
         apt-get install git-core gnupg flex bison gperf build-essential \
-		zip curl libc6-dev lib32ncurses5 libncurses5-dev x11proto-core-dev \
-		libx11-dev libreadline6-dev lib32readline-gplv2-dev libgl1-mesa-glx \
-		libgl1-mesa-dev g++-multilib mingw32 openjdk-6-jdk tofrodos \
-		python-markdown libxml2-utils xsltproc zlib1g-dev pngcrush \
-		libcurl4-gnutls-dev comerr-dev krb5-multidev libcurl4-gnutls-dev \
-		libgcrypt11-dev libglib2.0-dev libgnutls-dev libgnutls-openssl27 \
-		libgnutlsxx27 libgpg-error-dev libgssrpc4 libgstreamer-plugins-base0.10-dev \
-		libgstreamer0.10-dev libidn11-dev libkadm5clnt-mit8 libkadm5srv-mit8 \
-		libkdb5-6 libkrb5-dev libldap2-dev libp11-kit-dev librtmp-dev libtasn1-3-dev \
-		libxml2-dev tofrodos python-markdown lib32z-dev ia32-libs
-		ln -s /usr/lib32/libX11.so.6 /usr/lib32/libX11.so
-		ln -s /usr/lib32/libGL.so.1 /usr/lib32/libGL.so
+        zip curl libc6-dev lib32ncurses5 libncurses5-dev x11proto-core-dev \
+        libx11-dev libreadline6-dev lib32readline-gplv2-dev libgl1-mesa-glx \
+        libgl1-mesa-dev g++-multilib mingw32 openjdk-6-jdk tofrodos \
+        python-markdown libxml2-utils xsltproc zlib1g-dev pngcrush \
+        libcurl4-gnutls-dev comerr-dev krb5-multidev libcurl4-gnutls-dev \
+        libgcrypt11-dev libglib2.0-dev libgnutls-dev libgnutls-openssl27 \
+        libgnutlsxx27 libgpg-error-dev libgssrpc4 libgstreamer-plugins-base0.10-dev \
+        libgstreamer0.10-dev libidn11-dev libkadm5clnt-mit8 libkadm5srv-mit8 \
+        libkdb5-6 libkrb5-dev libldap2-dev libp11-kit-dev librtmp-dev libtasn1-3-dev \
+        libxml2-dev tofrodos python-markdown lib32z-dev ia32-libs
+        ln -s /usr/lib32/libX11.so.6 /usr/lib32/libX11.so
+        ln -s /usr/lib32/libGL.so.1 /usr/lib32/libGL.so
         ;;
         
     *)
@@ -192,24 +162,24 @@ prepare_environment()
     case $sources in
     "Y" | "y")
         echo "Choose a branch:"
-        echo "1) gingerbread"
-        echo "2) ics"
-        echo "3) jellybean"
-        echo "1) cm-10.1"
+        echo "1) cm-7 (gingerbread)"
+        echo "2) cm-9 (ics)"
+        echo "3) cm-10 (jellybean mr0)"
+        echo "4) cm-10.1 (jellybean mr1)"
         read -n1 branch
         echo -e "\r\n"
 
         case $branch in
             "1")
-                # gingerbread
+                # cm-7
                 branch="gingerbread"
                 ;;
             "2")
-                # ics
+                # cm-9
                 branch="ics"
                 ;;
             "3")
-                # jellybean
+                # cm-10
                 branch="jellybean"
                 ;;
             "4")
@@ -240,7 +210,9 @@ prepare_environment()
         mkdir -p $working_directory
         cd $working_directory
         repo init -u git://github.com/CyanogenMod/android.git -b $branch
-        repo sync
+        mkdir -p .repo/local_manifests
+        curl https://raw.github.com/teamhacksung/buildscripts/$branch/my_manifest.xml > $working_directory/.repo/local_manifests/my_manifest.xml
+        repo sync -j15
         echo "Sources synced to $working_directory"        
         exit
         ;;
@@ -313,6 +285,17 @@ echo -e "${txtblu} \r\n"
 echo -e "${txtblu} ###################################################################################################"
 echo -e "\r\n ${txtrst}"
 
+# Check for build target
+if [ -z "${CMD}" ]; then
+	echo -e "${txtred}No build target set."
+	echo -e "${txtred}Usage: ./build.sh i9300 (complete build)"
+	echo -e "${txtred}       ./build.sh i9300 kernel (bootimage only)"
+	echo -e "${txtred}       ./build.sh clean"
+    echo -e "${txtred}       ./build.sh prepare (prepares the build environment)"
+    echo -e "\r\n ${txtrst}"
+    exit
+fi
+
 # Starting Timer
 START=$(date +%s)
 
@@ -320,18 +303,19 @@ START=$(date +%s)
 case "$CMD" in
     prepare)
         check_root
+        check_machine_type
         prepare_environment
         exit
         ;;
-	clean)
-		make clean
-		rm -rf ./out/target/product
-		exit
-		;;
-	*)
-		lunch=cm_${CMD}-userdebug
+    clean)
+        make clean
+        rm -rf ./out/target/product
+        exit
+        ;;
+    *)
+        lunch=cm_${CMD}-userdebug
         brunch=${lunch}
-	    ;;
+        ;;
 esac
 
 # Get prebuilts once per day
@@ -371,8 +355,8 @@ cd -
 
 # Start the Build
 case "$EXTRACMD" in
-	kernel)
-		echo -e "${txtgrn}Rebuilding bootimage...${txtrst}"
+    kernel)
+        echo -e "${txtgrn}Rebuilding bootimage...${txtrst}"
 
         rm -rf ${ANDROID_PRODUCT_OUT}/obj/KERNEL_OBJ
         rm -rf ${ANDROID_PRODUCT_OUT}/kernel_zip
@@ -388,9 +372,9 @@ case "$EXTRACMD" in
             mka signapk
         fi
         create_kernel_zip
-		;;
-	recovery)
-		echo -e "${txtgrn}Rebuilding recoveryimage...${txtrst}"
+        ;;
+    recovery)
+        echo -e "${txtgrn}Rebuilding recoveryimage...${txtrst}"
 
         rm -rf ${ANDROID_PRODUCT_OUT}/obj/KERNEL_OBJ
         rm ${ANDROID_PRODUCT_OUT}/kernel
@@ -399,12 +383,12 @@ case "$EXTRACMD" in
         rm -rf ${ANDROID_PRODUCT_OUT}/ramdisk*
 
         mka ${ANDROID_PRODUCT_OUT}/recovery.img
-		;;
-	*)
-		echo -e "${txtgrn}Building Android...${txtrst}"
-		brunch ${brunch}
+        ;;
+    *)
+        echo -e "${txtgrn}Building Android...${txtrst}"
+        brunch ${brunch}
         create_kernel_zip
-		;;
+        ;;
 esac
 
 END=$(date +%s)
